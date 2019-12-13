179d8af8909205f00622ab99890f376bf1fffdd6
Pablo Avila <pablo98ad@gmail.com>
2019-12-13 09:28:28 +0100

    Controladores CRUD de todas las tablas

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,15 @@ Route::get('/ejemplo', function () {
     return view('ejemplo');
 });
 
-Route::get('/profesores', 'ProfesorController@index');
+Route::resource('profesor', 'ProfesorController');//para crear las rutas de las funciones del controlador de profesor
+Route::resource('grupo', 'GrupoController');//para crear las rutas de las funciones del controlador 
+Route::resource('alumno', 'AlumnoController');//para crear las rutas de las funciones del controlador 
+Route::resource('reservas', 'ReservasController');//para crear las rutas de las funciones del controlador
+Route::resource('ausencias', 'AusenciasController');//para crear las rutas de las funciones del controlador 
+Route::resource('anuncios', 'AnunciosController');//para crear las rutas de las funciones del controlador 
+Route::resource('horarios', 'HorarioController');//para crear las rutas de las funciones del controlador 
+Route::resource('materia', 'MateriaController');//para crear las rutas de las funciones del controlador 
+
+
+
+//Route::resource('telefono', 'TelefonoController');
